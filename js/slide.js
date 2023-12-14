2d422619179346f65fe9c052a6bf2ea8517806da
Barbara <bmariano2902@outlook.com>
2023-12-14 12:41:29 -0300

    Add imagens laravel

--- js/slide.js
+++ js/slide.js
@@ -16,8 +16,6 @@ const swiper2 = new Swiper(".mySwiper2", {
     slidesPerView: 'auto',
     speed: 3000,
     spaceBetween: 32,
-    freeMode: true,
-    // initialSlide: 0,
     autoplay: {
         "delay": 0,
         "disableOnInteraction": false,
